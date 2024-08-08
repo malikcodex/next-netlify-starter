2c82910d75fde720aaa9b1a06ba4baa5efbe878f
malikcodex <161372013+malikcodex@users.noreply.github.com>
2024-08-08 09:13:47 +0500

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 
-const About = () => {
+export default function Home() {
   return (
-    <div>about</div>
+    <div className="container">
+      <Head>
+        <title>Cars On Deal</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Welcome To The Cars On Deal Platform. We Are Working And Soon We Are Bringing The Module" />
+        <p className="description">
+          <a className="my-btn">Checkout Tools</a>
+        </p>
+      </main>
+
+      <Footer />
+    </div>
   )
 }
-
-export default About
